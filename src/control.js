e79f313f4928d2919a4bb0bd3fbd1a816278f8b6
m-szkudlarek <mateusz.szkudlarek1710@gmail.com>
2022-12-05 19:13:50 +0100

    working progress 05.12.2022

--- src/control.js
+++ src/control.js
@@ -6,10 +6,18 @@ const sidebar = document.querySelector(".sidebar");
 bntNavbar.forEach((bnt) =>
   bnt.addEventListener("click", function () {
     sidebar.classList.toggle("sidebar--open");
+
     if (header.classList.contains("header-sidebar--open"))
       setTimeout(function () {
         header.classList.remove("header-sidebar--open");
-      }, 700);
+      }, 600);
     else header.classList.add("header-sidebar--open");
+
+    if (sidebar.classList.contains("hidden"))
+      sidebar.classList.remove("hidden");
+    else
+      setTimeout(function () {
+        sidebar.classList.add("hidden");
+      }, 600);
   })
 );
